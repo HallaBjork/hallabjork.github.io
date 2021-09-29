b50ed9a148a310fcd142dc4fca561814e9b9bbc5
Halla Bjork Kristjansdottir <61651233+HallaBjork@users.noreply.github.com>
2021-09-29 15:51:29 +0100

    changes

--- CreativeModel/js/model.js
+++ CreativeModel/js/model.js
@@ -177,6 +177,9 @@ var model = {};
     ctx2.putImageData(bigimg.getCanvasImage(ctx), 0, 0);
     ctx2.scale(1, 1);
     ctx2.drawImage(canvas2, 0, 0);
+    var image_data = canvas2.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+    document.body.style["background-image"] = "url(" + image_data + ")";
+    document.body.style["background-position"] = "0% 0%"
   }
 
   function initSecondScreen(selection) {
